ef42b28f3e891a83cca4010d516feed4f89ebd4b
AndersonM15 <andersonmuoz241@hotmail.com>
2024-11-29 08:58:24 -0500

    Cambios Vista Productos

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -1,23 +1,30 @@
-import {View, Text, StyleSheet, Button} from 'react-native'
+import { View, Text, StyleSheet, Button } from "react-native";
 
-export const Product = ({navigation}) =>{
-    return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
-        <Button
-            title='HOME'
-            onPress={()=>{
-                navigation.navigate('HomeNav');
-            }}
-        />
+export const Product = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenido a mi tienda, soy Anderson Muño</Text>
+      <Text style={styles.product1}>Producto 1</Text>
+      <Text style={styles.product2}>Producto 2</Text>
+      <Text style={styles.product3}>Producto 3</Text>
+      <Button
+        title="HOME"
+        onPress={() => {
+          navigation.navigate("HomeNav");
+        }}
+      />
     </View>
-
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fffacd",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  product1: { color: "red" },
+  product2: { color: "blue" },
+  product3: { color: "green" },
+});
